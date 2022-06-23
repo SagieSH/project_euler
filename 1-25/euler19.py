93ec24cb1517ccf82cdb48c1401d2bfd31a8321d
SagieSH <sagie.shlfman@gmail.com>
2022-06-23 12:59:11 +0300

    Added new solutions

---
+++ 1-25/euler19.py
@@ -0,0 +1,40 @@
+
+import calendar
+
+
+DAYS_PER_MONTH = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
+
+
+def weekday(day, month, year):
+    year_difference = year - 1901
+    year_days = (365 * year_difference) + (year_difference // 4)
+    month_days = sum([DAYS_PER_MONTH[i] for i in range(1, month)])
+    if (year % 4 == 0) and (month > 2):
+        month_days += 1
+    return 1 + ((1 + (year_days + month_days + day)) % 7)
+
+
+counter = 0
+for year in range(1901, 2001):
+    print(year, weekday(1, 1, year), ((calendar.weekday(year, 1, 1) + 1) % 7) + 1)
+    for month in range(1, 13):
+        if weekday(1, month, year) == 1:
+            counter += 1
+
+
+print(counter)
+day, month, year = 6, 5, 2001
+print(year, weekday(day, month, year), ((calendar.weekday(year, month, day) + 1) % 7) + 1)
---
+++ 51-75/euler67.py
@@ -0,0 +1,30 @@
+
+from functools import lru_cache
+
+NUMBERS_PATH = "euler67_pyramid.txt"
+ROWS = 100
+PYRAMID = []
+
+
+def parse_pyramid():
+    global PYRAMID
+
+    with open(NUMBERS_PATH, 'r') as numbers_file:
+        for line in numbers_file.readlines():
+            if line[-1] == '\n':
+                line = line[:-1]
+            PYRAMID.append([int(num) for num in line.split()])
+    return PYRAMID
+
+
+@lru_cache
+def find_max_path(row, index):
+    my_val = PYRAMID[row][index]
+    if row == ROWS - 1:
+        return my_val
+    return my_val + max(find_max_path(row + 1, index), find_max_path(row + 1, index + 1))
+
+
+parse_pyramid()
+# The answer is 7273
+print(find_max_path(0, 0))
