df47e1b7b09e11fbc2012bc52587b0f2218878db
SagieSH <sagie.shlfman@gmail.com>
2023-09-02 20:56:44 +0300

    start of solution

---
+++ 26-50/euler33.py
@@ -0,0 +1,20 @@
+
+def minimize_fraction(numerator, denominator):
+
+
+
+numerator_product = 1
+denominator_product = 1
+for x in range(100, 1000):
+    big_numerator, big_denominator = x // 10, x % 100
+    small_numerator, small_denominator = x // 100, x % 10
+    try:
+        if (big_numerator != big_denominator) and (
+                (big_numerator / big_denominator) == (small_numerator / small_denominator)):
+            numerator_product *= small_numerator
+            denominator_product *= small_denominator
+    except ZeroDivisionError:
+        pass
+
+# The answer is: 100
+print(f"{numerator_product} / {denominator_product}")
